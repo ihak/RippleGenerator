92fc34b8e5f391cde7bfa89d21a48f2ed945a49b
Hassan Ahmed Khan <hassandotahmed@gmail.com>
2017-03-22 11:15:00 +0500

    Initial Commit

---
+++ RippleGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RippleGenerator
+//
+//  Created by Hassan Ahmed Khan on 3/22/17.
+//  Copyright © 2017 Hassan Ahmed Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
